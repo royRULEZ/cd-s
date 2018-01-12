e5e5473dc810d28203b0158c8a94345b3804660b
Roy Peckham <roypeckham@gmail.com>
2018-01-11 22:51:32 -0600

    1//1

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 module.exports ={
-    //url: 'mongodb://roy:1234@localhost/crypto' (!Mac)
-    url: 'mongodb://adminRoy:admingatorade12!@localhost/namr' //Live
+    
+    //url: 'mongodb://adminRoy:admingatorade12!@localhost/namr' //Live
+    url: 'mongodb://@localhost/crypto' //Mac,PC
+    
     //url: 'mongodb://adminRoy:admingatorade12!@52.15.54.43:27017/namr'
-    //url: 'mongodb://@localhost/crypto' //Mac,PC
+    //url: 'mongodb://roy:1234@localhost/crypto' (!Mac)
 };
